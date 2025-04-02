0d792ef74f964392f2b6d76830144d7ab9b3e298
EduardoTakeiYaginuma <eduyagnuma10@gmail.com>
2025-04-01 22:04:51 -0300

    Fix import routes

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,12 @@ import os
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()  
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{os.getenv("MD_DB_USERNAME")}:{os.getenv("MD_DB_PASSWORD")}@{os.getenv("MD_DB_SERVER")}/mydb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+from app import routes 
